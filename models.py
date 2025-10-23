859d014270696fdc7de6eb4a77f563ab06a925ab
Tikesh sahu <ts2665857@gmail.com>
2025-10-23 13:04:04 +0530

    xcgfvs

--- models.py
+++ models.py
@@ -20,3 +20,22 @@ class Product(db.Model):
     p_price = db.Column(db.Float(), nullable=False)
     p_amount = db.Column(db.Float(), nullable=False)
     p_special = db.Column(db.String(), nullable=False)
+    
+
+class Web(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)
+    templates = db.relationship('Template', backref='owner', lazy=False)
+    header_pages = db.relationship('Header_page',backref='owner', lazy=False )
+    is_content_editable = db.Column(db.Boolean, default = False)
+    preview_page  = db.Column(db.String(100), default="home.html")
+
+class Template(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    web_id = db.Column(db.Integer, db.ForeignKey('web.id'), nullable=False)
+    
+class Header_page(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    web_id = db.Column(db.Integer, db.ForeignKey('web.id'), nullable=False)
+    
